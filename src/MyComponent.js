37f41f0e7cbf11c4e84a46b681e11422ca021a30
amn305 <amanraj.ar206@gmail.com>
2024-09-02 20:04:40 +0530

    redux-thunk

---
+++ src/MyComponent.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchData } from './redux/actions';
+
+const MyComponent = () => {
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.data);
+  const isLoading = useSelector((state) => state.isLoading);
+  const error = useSelector((state) => state.error);
+  useEffect(() => {
+    dispatch(fetchData());
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  return (
+    <div>
+      {data.map((item) => (
+        <p key={item.id}>{item.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default MyComponent;
